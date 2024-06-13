2e504b0e83b17826d1377c0813753a27f037e1cd
Leliksey <57276673+Leliksey@users.noreply.github.com>
2024-06-13 16:16:03 +0300

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var title = document.getElementById("title");
-    title.addEventListener("click", function() {
-        this.innerHTML = "My Awesome Web Page";
-    })
-}
